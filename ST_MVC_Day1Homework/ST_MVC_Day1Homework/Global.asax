5a52327442e3aaee87ca3586643fc052e2e7a16f
taode <taode@LAPTOP-R946OL07>
2016-08-04 11:11:55 +0800

    初始化版本

---
+++ ST_MVC_Day1Homework/ST_MVC_Day1Homework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ST_MVC_Day1Homework.MvcApplication" Language="C#" %>
---
+++ ST_MVC_Day1Homework/ST_MVC_Day1Homework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ST_MVC_Day1Homework.Startup))]
+namespace ST_MVC_Day1Homework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
